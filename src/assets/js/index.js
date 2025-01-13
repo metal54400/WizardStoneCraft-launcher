defdac0827636ab3f321101928a9a8c8f87b31d6
metal54400 <audecillier@hotmail.fr>
2025-01-13 13:03:55 +0100

    V 2.0.2

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -30,13 +30,10 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Bienveunu sur WizardStoneCraftLauncher"},
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
-        this.splashAuthor.children[0].textContent = "@" + splash.author;
         await sleep(100);
         document.querySelector("#splash").style.display = "block";
         await sleep(500);
